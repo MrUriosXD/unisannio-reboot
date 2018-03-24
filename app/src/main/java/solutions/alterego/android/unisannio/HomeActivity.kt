8bd9df17ff210302206bceed615cc8b04e2ae285
Ivan Morgillo <imorgillo@gmail.com>
2018-03-24 10:11:36 +0100

    Add bottom nav click listeners

--- app/src/main/java/solutions/alterego/android/unisannio/HomeActivity.kt
+++ app/src/main/java/solutions/alterego/android/unisannio/HomeActivity.kt
@@ -2,6 +2,7 @@ package solutions.alterego.android.unisannio
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_home.bottom_navigation
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
@@ -10,16 +11,18 @@ import solutions.alterego.android.unisannio.core.onError
 import solutions.alterego.android.unisannio.core.onSuccess
 import timber.log.Timber
 
+
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        App.component(this).inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setSupportActionBar(findViewById(R.id.my_toolbar))
-        App.component(this).inject(this)
+        supportActionBar?.title = "Ateneo"
 
         launch(UI) {
-            val section = Giurisprudenza.sections[0]
+            val section = Giurisprudenza.sections.first()
             val elements = async { section.retriever.fetchItems() }.await()
 
             elements
@@ -33,5 +36,28 @@ class HomeActivity : AppCompatActivity() {
                 }
                 .onError { Timber.e(it) }
         }
+
+
+        bottom_navigation
+            .setOnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.navigation_ateneo -> {
+                        supportActionBar?.title = "Ateneo"
+                    }
+                    R.id.navigation_ingegneria -> {
+                        supportActionBar?.title = "Ingegneria"
+                    }
+                    R.id.navigation_scienze -> {
+                        supportActionBar?.title = "Scienze"
+                    }
+                    R.id.navigation_giurisprudenza -> {
+                        supportActionBar?.title = "Giurisprudenza"
+                    }
+                    R.id.navigation_economia -> {
+                        supportActionBar?.title = "S.E.A"
+                    }
+                }
+                true
+            }
     }
 }
